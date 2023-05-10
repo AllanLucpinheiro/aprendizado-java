0bba008eedeb5c709d37e40006470cbd6f4116a0
Allan Silva <allanlucpinheiro@gmail.com>
2023-05-10 19:01:16 -0300

    Questão em Java

---
+++ HoraDoSistema/src/main/java/horasosistema/HoraDoSistema.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package horasosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Allan
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é:");
+        System.out.println(relogio.toString());
+        
+    }
+}
