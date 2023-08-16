6f1add33740baadc4e848fbde7e052ee76c9a9d5
Allan Silva <allanlucpinheiro@gmail.com>
2023-08-15 21:48:46 -0300

    Aprendizado java e git

---
+++ repeticao01/src/main/java/com/mycompany/repeticao01/Repeticao01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repeticao01;
+
+/**
+ *
+ * @author Allan
+ */
+public class Repeticao01 {
+    
+    public static void main(String[] args) {
+    double a = Math.pow(8, 2);
+        System.out.println(a);
+    
+   
+}
+}
